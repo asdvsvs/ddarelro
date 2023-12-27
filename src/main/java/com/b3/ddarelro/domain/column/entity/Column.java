db4e1a834584477f09565b885d8bcd8d2b33bdf8
juneyoung Lee <junyeong237@gmail.com>
2023-12-27 20:29:18 +0900

    [#8] Refactor : 포맷팅수정

--- src/main/java/com/b3/ddarelro/domain/column/entity/Column.java
+++ src/main/java/com/b3/ddarelro/domain/column/entity/Column.java
@@ -13,7 +13,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
--- src/main/java/com/b3/ddarelro/domain/comment/entity/Comment.java
+++ src/main/java/com/b3/ddarelro/domain/comment/entity/Comment.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
