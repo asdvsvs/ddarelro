9de603f9fdb6e0500e9153d0f5399381e362163e
asdvsvs <gag973000@gmail.com>
2023-12-28 01:39:23 +0900

    [#4] Fix : 컬럼 삭제 소프트 딜리트로 변경 및 response 필드 변경

--- src/main/java/com/b3/ddarelro/domain/column/dto/response/ColumnDeleteRes.java
+++ src/main/java/com/b3/ddarelro/domain/column/dto/response/ColumnDeleteRes.java
@@ -3,6 +3,6 @@ package com.b3.ddarelro.domain.column.dto.response;
 import lombok.Builder;
 
 @Builder
-public record ColumnDeleteRes() {
+public record ColumnDeleteRes(String title, Boolean deleted) {
 
 }
