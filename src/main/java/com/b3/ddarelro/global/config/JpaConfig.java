33adf9e1b61a0b7eff7d7e88fa6f629fde224d58
gk <iba3374@naver.com>
2023-12-27 17:03:55 +0900

    feat: JpaAuditing 설정 추가

---
+++ src/main/java/com/b3/ddarelro/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.b3.ddarelro.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
