6e7044dbe42c2938364ac26ad3eb7718b7ec6d3e
asdvsvs <gag973000@gmail.com>
2023-12-27 21:21:05 +0900

    [#4] Feat : 컬럼 생성 api

--- src/main/java/com/b3/ddarelro/domain/column/controller/ColumnController.java
+++ src/main/java/com/b3/ddarelro/domain/column/controller/ColumnController.java
@@ -1,12 +1,28 @@
 package com.b3.ddarelro.domain.column.controller;
 
+import com.b3.ddarelro.domain.column.dto.request.ColumnCreateReq;
+import com.b3.ddarelro.domain.column.dto.response.ColumnCreateRes;
+import com.b3.ddarelro.domain.column.service.ColumnService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/columns")
 @RequiredArgsConstructor
 public class ColumnController {
 
+    private final ColumnService columnService;
+
+    @PostMapping
+    public ResponseEntity<ColumnCreateRes> createColumn(@RequestBody ColumnCreateReq req,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        ColumnCreateRes res = columnService.createColumn(req, userDetails.getUser().getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
 }
--- src/main/java/com/b3/ddarelro/domain/column/service/ColumnService.java
+++ src/main/java/com/b3/ddarelro/domain/column/service/ColumnService.java
@@ -1,12 +1,51 @@
 package com.b3.ddarelro.domain.column.service;
 
+import com.b3.ddarelro.domain.board.entity.Board;
+import com.b3.ddarelro.domain.board.service.BoardService;
+import com.b3.ddarelro.domain.column.dto.request.ColumnCreateReq;
+import com.b3.ddarelro.domain.column.dto.response.ColumnCreateRes;
+import com.b3.ddarelro.domain.column.entity.Column;
+import com.b3.ddarelro.domain.column.exception.ColumnErrorCode;
 import com.b3.ddarelro.domain.column.repository.ColumnRepository;
+import com.b3.ddarelro.domain.user.entity.User;
+import com.b3.ddarelro.global.exception.GlobalException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ColumnService {
-    private final ColumnRepository columnRepository;
 
+    private final ColumnRepository columnRepository;
+    private final BoardService boardService;
+    private final UserService userService;
+
+    public ColumnCreateRes createColumn(ColumnCreateReq req, Long userId) {
+        Board board = boardService.findBoard(req.boardId());
+        User user = userService.findUser(userId);
+        Long priority = columnRepository.count() + 1;
+        validateLeader(board, user);
+
+        Column column = columnRepository.save(Column.builder()
+            .title(req.title())
+            .board(board)
+            .priority(priority)
+            .build());
+
+        return ColumnCreateRes.builder()
+            .boardId(column.getBoard().getId())
+            .columnId(column.getId())
+            .title(column.getTitle())
+            .build();
+    }
+
+    private void validateLeader(Board board, User user) {
+        List<User> boardUsers = board.getUsers();
+        User boardLeader = boardUsers.stream().filter(u -> u.getAuthority().equals(LEADER))
+            .findFirst();
+        if (boardLeader != user) {
+            throw new GlobalException(ColumnErrorCode.INVALID_USER);
+        }
+    }
 }
