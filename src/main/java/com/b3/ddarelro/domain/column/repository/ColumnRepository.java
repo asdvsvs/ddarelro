7a51890e1d3f39d1b486488e33d3099702bf05a1
asdvsvs <gag973000@gmail.com>
2023-12-27 23:01:00 +0900

    [#4] Feat : 컬럼 조회 api

--- src/main/java/com/b3/ddarelro/domain/column/repository/ColumnRepository.java
+++ src/main/java/com/b3/ddarelro/domain/column/repository/ColumnRepository.java
@@ -1,8 +1,12 @@
 package com.b3.ddarelro.domain.column.repository;
 
 import com.b3.ddarelro.domain.column.entity.Column;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ColumnRepository extends JpaRepository<Column,Long> {
+public interface ColumnRepository extends JpaRepository<Column, Long> {
 
+    @Query("select c from Column c join fetch c.board where c.board = :id order by c.priority")
+    List<Column> findAllByBoardId(Long id);
 }
--- src/main/java/com/b3/ddarelro/domain/column/service/ColumnService.java
+++ src/main/java/com/b3/ddarelro/domain/column/service/ColumnService.java
@@ -3,7 +3,9 @@ package com.b3.ddarelro.domain.column.service;
 import com.b3.ddarelro.domain.board.entity.Board;
 import com.b3.ddarelro.domain.board.service.BoardService;
 import com.b3.ddarelro.domain.column.dto.request.ColumnCreateReq;
+import com.b3.ddarelro.domain.column.dto.request.ColumnGetReq;
 import com.b3.ddarelro.domain.column.dto.response.ColumnCreateRes;
+import com.b3.ddarelro.domain.column.dto.response.ColumnsGetRes;
 import com.b3.ddarelro.domain.column.entity.Column;
 import com.b3.ddarelro.domain.column.exception.ColumnErrorCode;
 import com.b3.ddarelro.domain.column.repository.ColumnRepository;
@@ -40,12 +42,26 @@ public class ColumnService {
             .build();
     }
 
+    public List<ColumnsGetRes> getColumns(ColumnGetReq req, Long userId) {
+        Board board = boardService.findBoard(req.boardId());
+        User user = userService.findUser(userId);
+        List<User> boardUsers = board.getUsers();
+        if (!boardUsers.contains(user)) {
+            throw new GlobalException(ColumnErrorCode.INVALID_USER);
+        }
+
+        List<Column> columns = columnRepository.findAllByBoardId(board.getId());
+
+        return columns.stream().map(column -> ColumnsGetRes.builder()
+            .title(column.getTitle()).build()).toList();
+    }
+
     private void validateLeader(Board board, User user) {
         List<User> boardUsers = board.getUsers();
         User boardLeader = boardUsers.stream().filter(u -> u.getAuthority().equals(LEADER))
             .findFirst();
         if (boardLeader != user) {
-            throw new GlobalException(ColumnErrorCode.INVALID_USER);
+            throw new GlobalException(ColumnErrorCode.NOT_LEADER);
         }
     }
 }
