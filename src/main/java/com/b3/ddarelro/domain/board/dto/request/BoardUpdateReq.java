5fd50e5ae03abce5337df520d0cc4f2bd1ee62bf
juneyoung Lee <junyeong237@gmail.com>
2023-12-27 22:38:24 +0900

    [#8] Refactor : Dto 수정

--- src/main/java/com/b3/ddarelro/domain/board/dto/request/BoardUpdateReq.java
+++ src/main/java/com/b3/ddarelro/domain/board/dto/request/BoardUpdateReq.java
@@ -1,22 +1,18 @@
 package com.b3.ddarelro.domain.board.dto.request;
 
 import com.b3.ddarelro.domain.board.entity.Color;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class BoardCreateReqDto {
-
+public class BoardUpdateReq{
     private Color color;
     private String name;
     private String description;
     private Boolean deleted;
 
     @Builder
-    private BoardCreateReqDto(Color color, String name, String description){
+    private BoardUpdateReq(Color color, String name, String description){
         this.color = color;
         this.deleted = false;
         this.name = name;
