73f5f897c341643b6a78e5fe21aea01a025235c7
asdvsvs <gag973000@gmail.com>
2023-12-27 23:01:00 +0900

    [#4] Feat : 컬럼 조회 api

--- src/main/java/com/b3/ddarelro/domain/column/controller/ColumnController.java
+++ src/main/java/com/b3/ddarelro/domain/column/controller/ColumnController.java
@@ -1,13 +1,17 @@
 package com.b3.ddarelro.domain.column.controller;
 
 import com.b3.ddarelro.domain.column.dto.request.ColumnCreateReq;
+import com.b3.ddarelro.domain.column.dto.request.ColumnGetReq;
 import com.b3.ddarelro.domain.column.dto.response.ColumnCreateRes;
+import com.b3.ddarelro.domain.column.dto.response.ColumnsGetRes;
 import com.b3.ddarelro.domain.column.service.ColumnService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +29,12 @@ public class ColumnController {
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         ColumnCreateRes res = columnService.createColumn(req, userDetails.getUser().getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ColumnsGetRes>> getColumns(@Valid @RequestBody ColumnGetReq req,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<ColumnsGetRes> res = columnService.getColumns(req, userDetails.getUser().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 }
