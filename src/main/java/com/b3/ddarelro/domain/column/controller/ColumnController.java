b28a990a4ca81765e4341175bb7c4c36c8bbb7de
asdvsvs <gag973000@gmail.com>
2023-12-27 23:48:22 +0900

    [#4] Feat : 컬럼 삭제 api, 중복 코드 메소드화

--- src/main/java/com/b3/ddarelro/domain/column/controller/ColumnController.java
+++ src/main/java/com/b3/ddarelro/domain/column/controller/ColumnController.java
@@ -1,9 +1,11 @@
 package com.b3.ddarelro.domain.column.controller;
 
 import com.b3.ddarelro.domain.column.dto.request.ColumnCreateReq;
+import com.b3.ddarelro.domain.column.dto.request.ColumnDeleteReq;
 import com.b3.ddarelro.domain.column.dto.request.ColumnGetReq;
 import com.b3.ddarelro.domain.column.dto.request.ColumnUpdateReq;
 import com.b3.ddarelro.domain.column.dto.response.ColumnCreateRes;
+import com.b3.ddarelro.domain.column.dto.response.ColumnDeleteRes;
 import com.b3.ddarelro.domain.column.dto.response.ColumnUpdateRes;
 import com.b3.ddarelro.domain.column.dto.response.ColumnsGetRes;
 import com.b3.ddarelro.domain.column.service.ColumnService;
@@ -13,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,11 +45,20 @@ public class ColumnController {
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
-    @PutMapping("/{columnId")
+    @PutMapping("/{columnId}")
     public ResponseEntity<ColumnUpdateRes> updateColumn(@PathVariable Long columnId,
         @Valid @RequestBody ColumnUpdateReq req,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         ColumnUpdateRes res = columnService.updateColumn(columnId, req,
+            userDetails.getUser().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
+    @DeleteMapping("/{columnId}")
+    public ResponseEntity<ColumnDeleteRes> deleteColumn(@PathVariable Long columnId,
+        @Valid @RequestBody ColumnDeleteReq req,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        ColumnDeleteRes res = columnService.deleteColumn(columnId, req,
             userDetails.getUser().getId());
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
---
+++ src/main/java/com/b3/ddarelro/domain/column/dto/request/ColumnDeleteReq.java
@@ -0,0 +1,7 @@
+package com.b3.ddarelro.domain.column.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record ColumnDeleteReq(@NotNull(message = "보드id가 null입니다") Long boardId) {
+
+}
