89354680d53541b5c31918569301c622fea916c6
juneyoung Lee <junyeong237@gmail.com>
2023-12-27 20:28:57 +0900

    [#8] Refactor : Board 수정

--- src/main/java/com/b3/ddarelro/domain/board/entity/Board.java
+++ src/main/java/com/b3/ddarelro/domain/board/entity/Board.java
@@ -1,12 +1,17 @@
 package com.b3.ddarelro.domain.board.entity;
 
-import com.b3.ddarelro.domain.card.entity.Card;
 import com.b3.ddarelro.domain.common.BaseEntity;
+import com.b3.ddarelro.domain.userboard.entity.UserBoard;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,16 +32,27 @@ public class Board extends BaseEntity {
 
     private Boolean deleted;
 
-    private ColorEnum color;
+    @Enumerated(EnumType.STRING)
+    private Color color;
+
+    @OneToMany(mappedBy = "board")
+    private List<UserBoard> userBoardList = new ArrayList<>();
 
     @Builder
-    private Board(String name, String description, ColorEnum color){
+    private Board(String name, String description, Color color){
         this.name = name;
         this.description = description;
         this.deleted = false;
         this.color = color;
     }
 
+    public void addUserBoard(UserBoard userBoard) {
+        this.userBoardList.add(userBoard);
+        userBoard.setBoard(this);
+    }
 
+    public void deleteBoardState(Boolean bool){
+        this.deleted = bool;
+    }
 
 }
