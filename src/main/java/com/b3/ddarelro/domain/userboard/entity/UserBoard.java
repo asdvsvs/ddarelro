7323e493ab5185cc29b83866cc544a2a12e6bcf9
juneyoung Lee <junyeong237@gmail.com>
2023-12-28 12:53:53 +0900

    [#8] Refactor : FetchType = LAZY추가

--- src/main/java/com/b3/ddarelro/domain/userboard/entity/UserBoard.java
+++ src/main/java/com/b3/ddarelro/domain/userboard/entity/UserBoard.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,11 +30,11 @@ public class UserBoard extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
 
