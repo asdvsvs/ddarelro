31daf01c7492162b161c692c6f7bce313c9f9f53
gk <iba3374@naver.com>
2023-12-28 17:40:21 +0900

    #[27] feat: 댓글 삭제, 복구 기능 서비스 분리, 댓글 응답 유저이름 nickname -> username으로 변경

--- src/main/java/com/b3/ddarelro/domain/comment/dto/response/CommentListRes.java
+++ src/main/java/com/b3/ddarelro/domain/comment/dto/response/CommentListRes.java
@@ -3,6 +3,6 @@ package com.b3.ddarelro.domain.comment.dto.response;
 import lombok.Builder;
 
 @Builder
-public record CommentListRes(Long id, String content, String nickname) {
+public record CommentListRes(Long id, String content, String username) {
 
 }
--- src/main/java/com/b3/ddarelro/domain/comment/service/CommentService.java
+++ src/main/java/com/b3/ddarelro/domain/comment/service/CommentService.java
@@ -68,23 +68,9 @@ public class CommentService {
             .map(comment -> CommentListRes.builder()
                 .id(comment.getId())
                 .content(comment.getContent())
-                .nickname(comment.getUser().getUsername())
+                .username(comment.getUser().getUsername())
                 .build())
             .toList();
-    }
-
-    /**
-     * Card가 delete할 때, 이 메소드를 호출 (상위에서 soft delete한 경우에만 카드도 soft delete를 실행)
-     */
-    public void deleteAllComment(List<Long> cardIds) {
-        commentRepository.SoftDelete(cardIds);
-    }
-
-    /**
-     * Card가 restore할 때 comment도 restore 한다.
-     */
-    public void restoreAllComment(List<Long> cardIds) {
-        commentRepository.restoreAll(cardIds);
     }
 
     private Comment findComment(Long commentId) {
