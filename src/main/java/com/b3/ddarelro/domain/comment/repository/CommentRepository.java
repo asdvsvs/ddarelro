dcd5007760afc5eb49ece7b28020dfb051cd4e52
gk <iba3374@naver.com>
2023-12-28 11:17:33 +0900

    [#16] feat: Card에서 SoftDelete를 할 때 필요한 deleteAll추가

--- src/main/java/com/b3/ddarelro/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/b3/ddarelro/domain/comment/repository/CommentRepository.java
@@ -1,10 +1,17 @@
 package com.b3.ddarelro.domain.comment.repository;
 
+import com.b3.ddarelro.domain.card.entity.Card;
 import com.b3.ddarelro.domain.comment.entity.Comment;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findAllByCardIdOrderByCreatedAtDesc(Long cardId);
+    List<Comment> findAllByCardOrderByCreatedAtDesc(Card card);
+
+    @Modifying
+    @Query(value = "update Comment c set c.deleted = true where c.card.id in (:cardIds)")
+    void SoftDelete(List<Long> cardIds);
 }
