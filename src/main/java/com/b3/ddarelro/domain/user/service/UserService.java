780deef4652aa8370a5b145540f527f4ee57acf1
seastamp <seastamp10@gmail.com>
2023-12-29 15:52:48 +0900

    [#40] Feat : User 회원탈퇴 구현

--- src/main/java/com/b3/ddarelro/domain/user/service/UserService.java
+++ src/main/java/com/b3/ddarelro/domain/user/service/UserService.java
@@ -4,12 +4,14 @@ package com.b3.ddarelro.domain.user.service;
 import com.b3.ddarelro.domain.user.dto.request.UserPasswordUpdateReq;
 import com.b3.ddarelro.domain.user.dto.request.UserSignupReq;
 import com.b3.ddarelro.domain.user.dto.request.UsernameUpdateReq;
+import com.b3.ddarelro.domain.user.dto.response.UserDeleteRes;
 import com.b3.ddarelro.domain.user.dto.response.UserPasswordUpdateRes;
 import com.b3.ddarelro.domain.user.dto.response.UserRes;
 import com.b3.ddarelro.domain.user.dto.response.UsernameUpdateRes;
 import com.b3.ddarelro.domain.user.entity.User;
 import com.b3.ddarelro.domain.user.exception.UserErrorCode;
 import com.b3.ddarelro.domain.user.repository.UserRepository;
+import com.b3.ddarelro.domain.userboard.repository.UserBoardRepository;
 import com.b3.ddarelro.global.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserBoardRepository userBoardRepository;
     private final PasswordEncoder passwordEncoder;
 
     public void signup(final UserSignupReq reqDto) {
@@ -79,8 +82,32 @@ public class UserService {
             .build();
     }
 
+    /**
+     * 회원탈퇴시 Soft Delete 처리를 하고 현재 참여하고 있는 보드 중 팀장 직급인 보드가 있으면 예외를 던진다.
+     */
+    // Todo: 일주일이 지나면 스케줄러를 돌려 email과 비밀번호를 임의의 값으로 수정한다.
+    // Todo: 1개월이 지나면 HardDelete를 한다.
+    @Transactional
+    public UserDeleteRes deleteUser(final Long userId) {
+        User foundUser = findUser(userId);
+
+        validateDeleteUser(foundUser);
+
+        foundUser.delete();
+        return UserDeleteRes.builder()
+            .message("탈퇴가 완료 되었습니다")
+            .build();
+    }
+
     public User findUser(final Long id) {
         return userRepository.findById(id)
             .orElseThrow(() -> new GlobalException(UserErrorCode.NOT_FOUND_USER));
+
+    }
+
+    private void validateDeleteUser(User user) {
+        if (userBoardRepository.countAdminUserBoards(user) > 0) {
+            throw new GlobalException(UserErrorCode.HAS_ADMIN_BOARD);
+        }
     }
 }
--- src/main/java/com/b3/ddarelro/domain/userboard/repository/UserBoardRepository.java
+++ src/main/java/com/b3/ddarelro/domain/userboard/repository/UserBoardRepository.java
@@ -3,8 +3,10 @@ package com.b3.ddarelro.domain.userboard.repository;
 import com.b3.ddarelro.domain.board.entity.Board;
 import com.b3.ddarelro.domain.user.entity.User;
 import com.b3.ddarelro.domain.userboard.entity.UserBoard;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserBoardRepository extends JpaRepository<UserBoard, Long> {
 
@@ -12,4 +14,8 @@ public interface UserBoardRepository extends JpaRepository<UserBoard, Long> {
 
     Boolean existsByUserAndBoard(User user, Board board);
 
+    List<UserBoard> findAllByUser(User foundUser);
+
+    @Query("select count(ub) from UserBoard ub where ub.user = :user and ub.boardAuthority = 'ADMIN'")
+    Long countAdminUserBoards(User user);
 }
