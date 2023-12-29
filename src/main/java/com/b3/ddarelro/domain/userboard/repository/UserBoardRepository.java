a0c8df3a441e33b7ba1f83d65be6cd8ff479b057
seastamp <seastamp10@gmail.com>
2023-12-29 19:28:51 +0900

    [#40] Refactor : userboardRepository 안쓰이는 메서드 삭제

--- src/main/java/com/b3/ddarelro/domain/userboard/repository/UserBoardRepository.java
+++ src/main/java/com/b3/ddarelro/domain/userboard/repository/UserBoardRepository.java
@@ -3,7 +3,6 @@ package com.b3.ddarelro.domain.userboard.repository;
 import com.b3.ddarelro.domain.board.entity.Board;
 import com.b3.ddarelro.domain.user.entity.User;
 import com.b3.ddarelro.domain.userboard.entity.UserBoard;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,8 +12,6 @@ public interface UserBoardRepository extends JpaRepository<UserBoard, Long> {
     Optional<UserBoard> findByUserAndBoard(User user, Board board);
 
     Boolean existsByUserAndBoard(User user, Board board);
-
-    List<UserBoard> findAllByUser(User foundUser);
 
     @Query("select count(ub) from UserBoard ub where ub.user = :user and ub.boardAuthority = 'ADMIN'")
     Long countAdminUserBoards(User user);
